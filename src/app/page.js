8f1296cdd093d7582dc9177826cd7a4bb02cf5c5
TahaFazlani <tahafazlani79@gmail.com>
2025-03-26 07:39:35 +0500

    change heading & add description

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HomePage from "./components/HomePage";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4 space-y-6">
+    <main className="container mx-auto p-4 space-y-6 bg-gradient-to-br from-blue-50 to-gray-100">
       <HomePage />
     </main>
   );
